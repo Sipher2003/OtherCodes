08a70054aca64d24c4a0530471b508e1edc2467b
Kaustubh <107745719+Sipher2003@users.noreply.github.com>
2023-01-08 19:37:00 +0530

    Create practice.cs

---
+++ C#/home/practice.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            /* Console.WriteLine("Enter a number:");
+             int a = int.Parse(Console.ReadLine());
+            String r= (a < 0) ? "Negative" : "Positive";
+             Console.WriteLine(r);*/
+
+
+            /*Console.WriteLine("Enter the Characters");
+            Char a = Char.Parse(Console.ReadLine());
+            Char b = Char.Parse(Console.ReadLine());
+            Char c = Char.Parse(Console.ReadLine());
+            Console.WriteLine("Original Format is {0} {1} {2}", a, b, c);
+            Console.WriteLine("Reversed Format is {0} {1} {2}", c, b, a); */
+
+
+           /*Console.WriteLine("enter 1st string:");
+            String a = Console.ReadLine();
+            Console.WriteLine("enter 2nd string:");
+            String b = Console.ReadLine();
+            Console.WriteLine("First String length is " + a.Length);
+            Console.WriteLine("String to upper is  " + a.ToUpper());
+            String concat = String.Concat(a, b);
+            Console.WriteLine(concat); */
+
+
+
+
+
+
+
+
+
+        }
+    }
+    
+}
