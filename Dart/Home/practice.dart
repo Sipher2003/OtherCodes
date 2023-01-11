8b11f99f5b2dd1ba61b9ff4c7b72a3e93075c4cb
Sipher <kaustubh.bhosale30@gmail.com>
2023-01-11 19:50:05 +0530

    Update practice.dart

--- Dart/Home/practice.dart
+++ Dart/Home/practice.dart
@@ -1,11 +1,34 @@
-import 'dart:io';
+// import 'dart:io';
  
 void main()
 {
-    print("Enter your name?");
+    // print("Enter your name?");
     
-    String? name = stdin.readLineSync();
+    // String? name = stdin.readLineSync();
  
     
-    print("Hello, $name! ");
+    // print("Hello, $name! ");
+    
+
+    // int a=10;
+    // double b=9.9;
+    // bool c=false;
+    // String d,e="Hello World";
+
+
+    // print(a);
+    // print(b);
+    // print(c);
+    // // print(d);
+    // print (e);
+
+
+    final str1="Hello World";
+    final String str2="Hello World  Again";
+
+    print(str1);
+    print(str2);
+
+
+
 }
