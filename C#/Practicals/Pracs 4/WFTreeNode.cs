57598938fb41066d98e81e58d6cdde9191bec33e
Sipher <kaustubh.bhosale30@gmail.com>
2023-01-16 22:31:39 +0530

    Create WFTreeNode.cs

---
+++ C#/Practicals/Pracs 4/WFTreeNode.cs
@@ -0,0 +1,36 @@
+/*
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       
+        <asp:TreeView ID="TreeView1" runat="server" Height="75px" Width="269px" BackColor="White" BorderColor="#CCFFCC" ForeColor="Maroon">
+            <Nodes>
+                <asp:TreeNode Text="Fybca" Value="Fybca">
+                    <asp:TreeNode Text="Web development" Value="Web development"></asp:TreeNode>
+                    <asp:TreeNode Text="Java" Value="Java"></asp:TreeNode>
+                </asp:TreeNode>
+                <asp:TreeNode Text="Sybca" Value="Sybca">
+                    <asp:TreeNode Text="AI" Value="AI" NavigateUrl="~/home page.aspx"></asp:TreeNode>
+                    <asp:TreeNode Text="Cn" Value="Cn"></asp:TreeNode>
+                </asp:TreeNode>
+                <asp:TreeNode Text="Tybca" Value="Tybca">
+                    <asp:TreeNode Text="MERN" Value="MERN"></asp:TreeNode>
+                    <asp:TreeNode Text="Awp" Value="Awp"></asp:TreeNode>
+                </asp:TreeNode>
+            </Nodes>
+        </asp:TreeView>
+       
+    </form>
+</body>
+</html>
+
+
+*/
