5780e0561bd8519876ab0a7204f9c10dc510090f
Kaustubh <107745719+Sipher2003@users.noreply.github.com>
2023-03-11 20:23:19 +0530

    Create practiceflutter.dart

---
+++ Dart/Home/practiceflutter.dart
@@ -0,0 +1,51 @@
+//basic hello world with bottomnavbar and elevated button
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Hello there',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: Myhomepage(),
+//     );
+//   }
+// }
+
+// class Myhomepage extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         backgroundColor: Colors.black,
+//         appBar: AppBar(title: Text('First app')),
+//         body: Center(
+//             child: Text(
+//           'Hello World',
+//           style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 40),
+//         )),
+//         floatingActionButton: FloatingActionButton(
+//             elevation: 10.0,
+//             backgroundColor: Colors.white,
+//             foregroundColor: Colors.red,
+//             child: Icon(Icons.add),
+//             onPressed: () {
+// // action on button press
+//             }),
+//         bottomNavigationBar: BottomNavigationBar(
+//           currentIndex: 0,
+//           fixedColor: Colors.orange,
+//           items: [
+//             BottomNavigationBarItem(label: ("Home"), icon: Icon(Icons.home)),
+//             BottomNavigationBarItem(
+//                 label: ("Search"), icon: Icon(Icons.search)),
+//             BottomNavigationBarItem(
+//                 label: ("Cart"), icon: Icon(Icons.shopping_cart))
+//           ],
+//         ));
+//   }
+// }
+
+
+
+
