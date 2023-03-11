c7c881467015b30023e2067e088d86a2c728f546
Kaustubh <107745719+Sipher2003@users.noreply.github.com>
2023-03-11 21:05:55 +0530

    Update practiceflutter.dart

--- Dart/Home/practiceflutter.dart
+++ Dart/Home/practiceflutter.dart
@@ -1,4 +1,4 @@
-//basic hello world with bottomnavbar and elevated button
+//basic hello world with bottomnavbar and floatingaction button
 // import 'package:flutter/material.dart';
 
 // void main() => runApp(MyApp());
@@ -47,5 +47,67 @@
 // }
 
 
+
+
+// Example of listtile
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Hello there',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: Myhomepage(),
+//     );
+//   }
+// }
+
+// class Myhomepage extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         // backgroundColor: Colors.black,
+//         appBar: AppBar(title: Text('First app')),
+//         body: ListView(
+//           children: [
+//             ListTile(
+//               title: Text('This is main text'),
+//               subtitle: Text('this is  subtext'),
+//               leading: Icon(Icons.add),
+//               trailing: Icon(Icons.arrow_forward),
+//               contentPadding: EdgeInsets.all(10),
+//             ),
+//             ListTile(
+//               title: Text('This is main text'),
+//               subtitle: Text('this is  subtext'),
+//               leading: Icon(Icons.add),
+//               trailing: Icon(Icons.arrow_forward),
+//               contentPadding: EdgeInsets.all(10),
+//             )
+//           ],
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//             elevation: 10.0,
+//             backgroundColor: Colors.white,
+//             foregroundColor: Colors.red,
+//             child: Icon(Icons.add),
+//             onPressed: () {
+// // action on button press
+//             }),
+//         bottomNavigationBar: BottomNavigationBar(
+//           currentIndex: 0,
+//           fixedColor: Colors.orange,
+//           items: [
+//             BottomNavigationBarItem(label: ("Home"), icon: Icon(Icons.home)),
+//             BottomNavigationBarItem(
+//                 label: ("Search"), icon: Icon(Icons.search)),
+//             BottomNavigationBarItem(
+//                 label: ("Cart"), icon: Icon(Icons.shopping_cart))
+//           ],
+//         ));
+//   }
+// }
 
 
