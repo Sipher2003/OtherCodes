59d666e4cce6dac94c296b4e62c92a328d00cf4f
Kaustubh <107745719+Sipher2003@users.noreply.github.com>
2023-03-11 23:25:18 +0530

    Update practiceflutter.dart

--- Dart/Home/practiceflutter.dart
+++ Dart/Home/practiceflutter.dart
@@ -111,3 +111,74 @@
 // }
 
 
+
+
+
+
+
+//textfield
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Hello there',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: Myhomepage(),
+//     );
+//   }
+// }
+
+// class Myhomepage extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         // backgroundColor: Colors.black,
+//         appBar: AppBar(title: Text('First app')),
+//         body: Padding(
+//             padding: const EdgeInsets.all(20),
+//             child: Column(
+//               children: [
+//                 TextField(
+//                     decoration: InputDecoration(
+//                         border: InputBorder.none,
+//                         labelText: 'Name',
+//                         hintText: 'Enter your name',
+//                         filled: true)),
+//                 SizedBox(height: 20),
+//                 TextField(
+//                     decoration: InputDecoration(
+//                         border: InputBorder.none,
+//                         labelText: 'class',
+//                         hintText: 'Enter your class',
+//                         filled: true)),
+//                 SizedBox(height: 20),
+//                 ElevatedButton(
+//                   onPressed: () {},
+//                   child: const Text('Fetch'),
+//                 )
+//               ],
+//             )),
+//         floatingActionButton: FloatingActionButton(
+//             elevation: 10.0,
+//             backgroundColor: Colors.white,
+//             foregroundColor: Colors.red,
+//             child: Icon(Icons.add),
+//             onPressed: () {
+// // action on button press
+//             }),
+//         bottomNavigationBar: BottomNavigationBar(
+//           currentIndex: 0,
+//           fixedColor: Colors.orange,
+//           items: [
+//             BottomNavigationBarItem(label: ("Home"), icon: Icon(Icons.home)),
+//             BottomNavigationBarItem(
+//                 label: ("Search"), icon: Icon(Icons.search)),
+//             BottomNavigationBarItem(
+//                 label: ("Cart"), icon: Icon(Icons.shopping_cart))
+//           ],
+//         ));
+//   }
+// }
