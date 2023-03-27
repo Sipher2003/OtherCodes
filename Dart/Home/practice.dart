ad75e321dd6fed2f7a73f34bef91478b5d134773
Sipher <kaustubh.bhosale30@gmail.com>
2023-03-27 20:02:37 +0530

    Update practice.dart

--- Dart/Home/practice.dart
+++ Dart/Home/practice.dart
@@ -19,7 +19,67 @@ void main(){
 // print("Sum of a and b is ${a+b}");
 
 
-num a=10;
-print(a);
+// num a=10;
+// print(a);
+
+
+// String a="Hello";
+// String b="World";
+
+// print(a+b);
+
+// bool val=(a!=b);
+// print(val);
+
+// List a=["Hello",1,78,9.39,true];
+
+// for (var hell in a) {
+//   print(hell);
+// }
+
+
+// int num1=30;
+// int num2=20;
+// int num3=60;
+
+// if(num1>num2){
+//   if(num1>num3){
+//     print("num1 is greatest");
+//   }
+//   else{
+//     print("num3 is greatest");
+//   }
+// }
+// else{
+//   if(num2>num3){
+//     print("num2 is greatest");
+//   }
+//   else{
+//     print("num3 is greatest");
+//   }
+
+var grade="A";
+
+
+switch(grade){
+
+case "A":{
+  print("100");
+  break;
+}
+
+case "B":{
+  print("200");
+  break;
+}
+
+default:{
+  print("invald");
+}
+
+
+}
+
+
 
 }
