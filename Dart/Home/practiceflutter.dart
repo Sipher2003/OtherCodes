61a58e6b645264b67dd51acda8cfad20cfe336b8
Kaustubh <107745719+Sipher2003@users.noreply.github.com>
2023-04-06 06:08:46 +0530

    Update practiceflutter.dart

--- Dart/Home/practiceflutter.dart
+++ Dart/Home/practiceflutter.dart
@@ -615,3 +615,66 @@
 // }
 
 
+
+//squareanimation
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+// const img_url =
+//     'https://www.pngkey.com/png/detail/61-612294_anime-manga-animeboy-one-piece-luffy-manga-anime.png';
+
+// class MyApp extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "hello",
+//       theme: ThemeData(brightness: Brightness.dark),
+//       home: Myhome(),
+//     );
+//   }
+// }
+
+// class Myhome extends StatefulWidget {
+//   Myhomestate createState() => Myhomestate();
+// }
+
+// double _height = 100;
+// double _width = 100;
+// Color _color = Colors.red;
+
+// class Myhomestate extends State<Myhome> {
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           AnimatedContainer(
+//             duration: Duration(seconds: 4),
+//             height: _height,
+//             width: _width,
+//             color: _color,
+//           )
+//         ],
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: update,
+//         child: Icon(Icons.play_arrow),
+//       ),
+//     );
+//   }
+
+//   void update() {
+//     setState(() {
+//       if (_color == Colors.red) {
+//         _height = 200;
+//         _width = 200;
+//         _color = Colors.blue;
+//       } else {
+//         _height = 100;
+//         _width = 100;
+//         _color = Colors.red;
+//       }
+//     });
+//   }
+// }
+
+
+
