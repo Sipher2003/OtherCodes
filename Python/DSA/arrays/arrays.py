bb154292aa95b701e7f2f45c935ff148569e40ec
Kaustubh <kaustubh.bhosale30@gmail.com>
2023-08-16 08:40:46 +0530

    new

---
+++ Python/DSA/arrays/arrays.py
@@ -0,0 +1,33 @@
+
+# odd2=[]
+
+# def oddappend(num):
+#     odd2.clear()
+#     for i in range(num):
+#         if(i%2==1):
+#             odd2.append(i)
+    
+#     print(odd2)
+
+
+# oddappend(30)
+
+# heros=['spider man','thor','hulk','iron man','captain america']
+
+
+# heros.append("black panther")
+# print(len(heros))
+
+# heros.remove("black panther")
+# print(len(heros))
+
+# heros.insert(3,"black panther")
+# print(heros)
+
+# heros[1:3]=["doctor strange"]
+# print(heros)
+
+
+
+
+
