948f63c90afa092115fc234b9fb505a5df19af35
Kaustubh <107745719+Sipher2003@users.noreply.github.com>
2024-11-20 12:17:13 +0530

    Update Second.java

--- Java/new java/Second.java
+++ Java/new java/Second.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 
 public class Second {
-  
+
     public static void main(String[] args) {
-        System.out.println("Whats ypur name: ");
-        Scanner input = new Scanner(System.in);
-        System.out.println("Hello "+input.next());
+
+        Scanner inp = new Scanner(System.in);
+
+        int a = inp.nextInt();
+        int b = inp.nextInt();
+        int c = inp.nextInt();
+
+        int max = 0;
+
+        if (a > b) {
+            max = a;
+        } else {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+
+        System.out.println(max);
+
+
 
     }
 
