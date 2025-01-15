311d67d42276ee187062b7aa8407ac7d0aa3a690
Kaustubh <107745719+Sipher2003@users.noreply.github.com>
2025-01-15 09:22:08 +0530

    Update Pattern1.java

--- Java/new java/Patterns/Pattern1.java
+++ Java/new java/Patterns/Pattern1.java
@@ -7,7 +7,9 @@ public class Pattern1 {
         // ptrn3(5);
         // ptrn4(5);
         // ptrn5(5);
-        ptrn6(5);
+        // ptrn6(5);
+        // ptrn7(5);
+        ptrn8(5);
     }
 
     static void ptrn1(int n) {
@@ -96,22 +98,22 @@ public class Pattern1 {
     }
 
     static void ptrn6(int n) {
-    //      *
-    //     * *
-    //    * * *
-    //   * * * *
-    //  * * * * *
-    //   * * * *
-    //    * * *
-    //     * *
-    //      *
+        // *
+        // * *
+        // * * *
+        // * * * *
+        // * * * * *
+        // * * * *
+        // * * *
+        // * *
+        // *
 
         for (int rows = 1; rows < 2 * n; rows++) {
             int totalcols = rows > n ? n - (rows - n) : rows;
 
-            int totalspaces= n-totalcols;
+            int totalspaces = n - totalcols;
             for (int s = 1; s <= totalspaces; s++) {
-                System.out.print(" ");
+                System.out.print("-");
             }
 
             for (int cols = 1; cols <= totalcols; cols++) {
@@ -119,6 +121,56 @@ public class Pattern1 {
             }
             System.out.println();
         }
+    }
+
+    static void ptrn7(int n) {
+        // 1
+        // 2 1 2
+        // 3 2 1 2 3
+        // 4 3 2 1 2 3 4
+        // 5 4 3 2 1 2 3 4 5
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    static void ptrn8(int n) {
+
+        // 1
+        // 212
+        // 32123
+        // 4321234
+        // 32123
+        // 212
+        // 1
+
+        for (int i = 1; i <= 2 * n; i++) {
+            int totalcols = i > n ? n - (i - n) : i;
+
+            for (int s = 1; s <= n - totalcols; s++) {
+                System.out.print(" ");
+            }
+            for (int j = totalcols; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= totalcols; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
     }
 
 }
