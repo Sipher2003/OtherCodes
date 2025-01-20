0528b7aceaafda93a23cd6ad6e428ef03e1f9ec2
Kaustubh <107745719+Sipher2003@users.noreply.github.com>
2025-01-20 20:46:43 +0530

    bubblesort

---
+++ Java/new java/Bubble Sort/BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] arr={11,23,9,16,2};
+
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void bubble(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+
+                }
+            }
+        }
+
+    }
+
+}
