814c62130b41462e786f1865300790401b55f7fe
Kaustubh <107745719+Sipher2003@users.noreply.github.com>
2025-01-20 20:47:21 +0530

    Update BubbleSort.java

--- Java/new java/Bubble Sort/BubbleSort.java
+++ Java/new java/Bubble Sort/BubbleSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int[] arr={11,23,9,16,2};
+        int[] arr={113,243,923,1326,112};
 
         bubble(arr);
         System.out.println(Arrays.toString(arr));
