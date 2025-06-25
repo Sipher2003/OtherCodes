0657bdafa293c3e16cb0a35fade923bba9bbb063
Kaustubh <107745719+Sipher2003@users.noreply.github.com>
2025-06-25 20:19:17 +0530

    Update Permutation.java

--- Java/new java/Recursion/Permutations/Permutation.java
+++ Java/new java/Recursion/Permutations/Permutation.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class Permutation {
 
     public static void main(String[] args) {
-        Perms("", "abc");
+        // Perms("", "abc");
+        System.out.println(Perms2("", "abc"));
     }
 
     static void Perms(String p,String up){
@@ -18,5 +21,27 @@ public class Permutation {
             Perms(f+ch+s,up.substring(1));
         }
 
+    }
+
+
+    //return in an Arraylist
+    static ArrayList<String> Perms2(String p,String up){
+        if(up.isEmpty()){
+            // System.out.println(p);
+            // return;
+            ArrayList<String>list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+            ArrayList<String>ans=new ArrayList<>();
+
+        char ch=up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f=p.substring(0, i);
+            String s=p.substring(i,p.length());
+            ans.addAll(Perms2(f+ch+s, up.substring(1)));
+        }
+        return ans;
     }
 }
